ce223a99cdb5c7af7194c6993244d6b16f320cca
OKrylov <krylov92oa@gmail.com>
2020-04-23 23:03:40 +0300

    Initial commit

---
+++ src/ru/geekbrains/ads/lesson1/Test.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.ads.lesson1;
+
+public class Test {
+
+    private static int staticA;
+
+    public static void main(String[] args) {
+        int a = 5;//a->001[5]
+        System.out.println(a);
+        System.out.println(staticA);
+
+        int[] array1 = new int[5];
+        Integer[] array2 = new Integer[5];
+
+    }
+}
